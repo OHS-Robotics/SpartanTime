f9af817ab1a239d33b382b98e125f4513f4e1832
Coda <102481816+CodaCM@users.noreply.github.com>
2025-10-13 11:01:25 -0500

    import and implementation major upgrade
    Fixed many issues and updated some implementations and imports

--- src/main/java/databases/DatabaseProcess.java
+++ src/main/java/databases/DatabaseProcess.java
@@ -9,7 +9,7 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
@@ -36,7 +36,7 @@ public class DatabaseProcess {
    */
   private static final String APPLICATION_NAME = "GrizzlyTime JavaFX Edition";
 
-  private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+  private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
   private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
   private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
